172ec1b6e462c18baf7218be11a53d6c5427a835
David <davidparro@gmail.com>
2016-12-18 15:57:45 +0100

    Revert "Revert "accesoTest()""
    This reverts commit 165667b46afe273a42653a51efeb55a108e8b23c.

--- inc/funciones.inc.php
+++ inc/funciones.inc.php
@@ -80,4 +80,36 @@
 	 function validarEmail (){
          filter_var('bob@example.com', FILTER_VALIDATE_EMAIL);
      } 
+
+    function accesoTest($nif,$clave){
+        /*true(1), si no tiene nota. false(-1) usuario no existe. False (-2)
+            clave incorrecta. False(-3) si ya tiene nota.*/
+        if (file_exists("./registro.dat.php")) {
+            $lineasFichero = file("./registro.dat.php", FILE_IGNORE_NEW_LINES);
+            for ($i=0; $i < sizeof($lineasFichero); $i++) { 
+                $usuarios[] = explode('*',$lineasFichero[$i]);									
+            }
+
+            for ($j=0; $j < sizeof($usuarios); $j++) { 
+                if ($usuario == $usuarios[$j][0] && $clave == $usuarios[$j][1]) {
+                    if($usuarios[$j][3]== "-"){
+                        return 1; //usuario es correcto
+                    }else{
+                        return -3; //Si ya tiene nota puesta
+                    }
+                }else{
+                    return -2; //Clave o usuario incorrecto
+                }
+            }
+            return -1; //Usuario no encontrado
+        }else{
+            return -4; //fichero no existe
+        }
+    }
+
+    function obtenerRespuestas(array[datos]){
+        /*obtenerRespuestas(array[datos]){}Guardamos en un array las respuestas del test y se
+        comparan con el array de respuestas correctas para tener la nota. Llama a validarNota para
+        guardarla.*/
+    }
 ?>
